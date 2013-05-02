8110e28a0bb7ac5df6ca0597f3edba0da7866911
Alex Todaro <alextodaro@rocketmail.com>
2013-05-02 09:42:59 -0700

    First commit

---
+++ src/com/rocketmail/alextodaro/TeleItems/Main.java
@@ -0,0 +1,113 @@
+package com.rocketmail.alextodaro.TeleItems;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	public File locationFile;
+	public YamlConfiguration locations;
+	public int size;
+	public static Inventory locInv;
+	public static HashMap<Integer, Location> itemLocations = new HashMap<Integer, Location>();
+	public static ItemStack teleMap = new ItemStack(395);
+
+	public void saveLocationFile() {
+		Bukkit.getServer().getPluginManager()
+				.registerEvents(new TeleListener(), this);
+
+		ItemMeta tmim = teleMap.getItemMeta();
+		List<String> tmLoreList = new ArrayList<String>();
+		tmLoreList.add("Handy Teleporter!");
+		tmim.setLore(tmLoreList);
+		tmim.setDisplayName("Teleporter");
+		teleMap.setItemMeta(tmim);
+
+		try {
+			locations.save(locationFile);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void onEnable() {
+		try {
+			locationFile = new File(getDataFolder(), "locations.yml");
+			if (!locationFile.exists()) {
+				locationFile.getParentFile().mkdirs();
+				locationFile.createNewFile();
+			}
+			locations = YamlConfiguration.loadConfiguration(locationFile);
+			if (!locations.contains("inventorysize")) {
+				locations.set("inventorysize", 1);
+			}
+			if (!locations.contains("inventoryname")) {
+				locations.set("inventoryname", "Locations");
+			}
+			if (!locations.contains("items")) {
+				ConfigurationSection items = locations.createSection("items");
+				for (int size = (locations.getInt("inventorysize") * 9), i = 0; i < size; i++) {
+					if (!items.contains("" + i)) {
+						ConfigurationSection sec = items.createSection("" + i);
+						sec.set("id", "0");
+						sec.set("name", "");
+						sec.set("location.x", "0");
+						sec.set("location.y", "0");
+						sec.set("location.z", "0");
+						sec.set("location.world", "");
+						sec.set("description", "");
+					}
+				}
+
+			}
+			saveLocationFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		size = locations.getInt("inventorysize") * 9;
+		locInv = Bukkit.createInventory(null, size,
+				locations.getString("inventoryname"));
+		ConfigurationSection items = locations.getConfigurationSection("items");
+		for (int i = 0; i < size; i++) {
+			ConfigurationSection sec = items.getConfigurationSection("" + i);
+			int id = Integer.valueOf(sec.getString("id"));
+			String name = sec.getString("name");
+			int x = Integer.valueOf(sec.getString("location.x"));
+			int y = Integer.valueOf(sec.getString("location.y"));
+			int z = Integer.valueOf(sec.getString("location.z"));
+			String world = sec.getString("location.world");
+			String lores = sec.getString("description");
+			if (id != 0 && Material.getMaterial(id) != null
+					&& name.length() > 0) {
+				ItemStack is = new ItemStack(id);
+				ItemMeta im = is.getItemMeta();
+				im.setDisplayName(name);
+				List<String> lorelist = new ArrayList<String>();
+				for (String s : lores.split(",")) {
+					lorelist.add(s);
+				}
+				im.setLore(lorelist);
+				is.setItemMeta(im);
+				locInv.setItem(i, is);
+				World w = Bukkit.getWorld(world);
+				getLogger().info("Loading location " + i + ", " + w.getName() + ", " + x + ", " + y + ", "  + z);
+				itemLocations.put(Integer.valueOf(i), new Location(w, x, y, z));
+			}
+
+		}
+
+	}
+}
---
+++ src/com/rocketmail/alextodaro/TeleItems/TeleListener.java
@@ -0,0 +1,111 @@
+package com.rocketmail.alextodaro.TeleItems;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class TeleListener implements Listener {
+
+	@EventHandler
+	public void onClick(PlayerInteractEvent event) {
+		if (event.getAction() == Action.RIGHT_CLICK_AIR
+				|| event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			ItemStack is = event.getPlayer().getItemInHand();
+			if (is.equals(Main.teleMap)) {
+				event.setCancelled(true);
+				Player player = event.getPlayer();
+				if (player.hasPermission("ti.map.use")) {
+					player.sendMessage(ChatColor.GREEN
+							+ "Click on an item on the top to teleport!");
+					player.openInventory(Main.locInv);
+				} else {
+					player.sendMessage("You do not have permission to do that!");
+				}
+			}
+		}
+
+		if (event.getAction() == Action.LEFT_CLICK_BLOCK) {
+			if (event.getClickedBlock().getType() == Material.WALL_SIGN
+					|| event.getClickedBlock().getType() == Material.SIGN_POST
+					&& event.getClickedBlock().getState() instanceof Sign) {
+				Sign s = (Sign) event.getClickedBlock().getState();
+				if (ChatColor.stripColor(s.getLine(1)).equals("[Teleporter]")) {
+					Player player = event.getPlayer();
+					if (player.hasPermission("ti.sign.use")) {
+						player.getInventory().addItem(Main.teleMap);
+						player.sendMessage(ChatColor.GREEN
+								+ "Click with this map to teleport from place to place!");
+					} else {
+						player.sendMessage(ChatColor.RED
+								+ "You do not have permission to do that!");
+					}
+				}
+
+			}
+		}
+	}
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		if (!event.getPlayer().hasPlayedBefore()
+				&& event.getPlayer().hasPermission("ti.map.login")) {
+			event.getPlayer().getInventory().addItem(Main.teleMap);
+			event.getPlayer()
+					.sendMessage(
+							ChatColor.GREEN
+									+ "Click with this map to teleport from place to place!");
+		}
+	}
+
+	@EventHandler
+	public void onSignWrite(SignChangeEvent event) {
+		Player player = event.getPlayer();
+		Block block = event.getBlock();
+		if (block.getState() instanceof Sign) {
+			Sign sign = (Sign) block.getState();
+			if (event.getLine(1).equalsIgnoreCase("[teleporter]")) {
+				if (player.hasPermission("ti.sign.create")) {
+
+					event.setLine(1, ChatColor.GREEN + "[Teleporter]");
+					event.setLine(0, "");
+					sign.setLine(1, ChatColor.GREEN + "[Teleporter]");
+					sign.update();
+
+					player.sendMessage(ChatColor.GREEN
+							+ "Sign created successfully!");
+				} else {
+					player.sendMessage(ChatColor.RED
+							+ "You do not have permission to do that!");
+					sign.getBlock().breakNaturally();
+				}
+			}
+		}
+	}
+
+	@EventHandler
+	public void onClick(InventoryClickEvent event) {
+		Player player = (Player) event.getWhoClicked();
+		Inventory invOpen = event.getInventory();
+		if (invOpen.getName().equals(Main.locInv.getName())) {
+			int slot = event.getSlot();
+			event.setCancelled(true);
+			Integer i = Integer.valueOf(slot);
+			Location teleLoc = Main.itemLocations.get(i);
+			player.teleport(teleLoc);
+		}
+
+	}
+
+}
